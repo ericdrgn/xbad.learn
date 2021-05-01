f6689b04140434c2777c93754fdc515cc885267c
ericdrgn <60805441+ericdrgn@users.noreply.github.com>
2021-05-01 04:40:02 -0600

    Create LogoBanner.vue

---
+++ src/components/LogoBanner.vue
@@ -0,0 +1,22 @@
+<template>
+<img src="/logobanner.png" alt="Dragon" style="width:202px;height:30px;">
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 200
+    }
+  },
+  computed: {
+    getWidth() {
+      return this.width;
+    },
+    getHeight() {
+      return Math.ceil((this.getWidth / 48) * 48);
+    }
+  }
+};
+</script>
